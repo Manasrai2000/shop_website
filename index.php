8f59a11941fadc56aca7716a3843f34e8817e6cb
Manasrai2000 <manasrai123400@gmail.com>
2023-04-29 18:31:59 +0530

    explore more section done

--- index.php
+++ index.php
@@ -31,45 +31,61 @@
     </div>
   </div>
   <!-- hero -->
-  
-  
-  
+
+
+
   <!-- Explore more -->
   <h1 class="header">Explore more</h1>
-  <div class="row">
-    <div class="card column">
-      <p>Starters Section</p>
+  <div class="rowex">
+        <div class="colex">
+            <a href="#">
+                <div class="card" style="background-image: url('./images/13.jpg');">
+                    <p>Starters Section</p>
+                </div>
+            </a>
+        </div>
+        <div class="colex">
+            <a href="#">
+                <div class="card" style="background-image: url('./images/13.jpg');">
+                    <p>Beaverages</p>
+                </div>
+            </a>
+        </div>
+        <div class="colex">
+            <a href="#">
+                <div class="card" style="background-image: url('./images/13.jpg');">
+                    <p>Full course meal</p>
+                </div>
+            </a>
+        </div>
+        <div class="colex">
+            <a href="#">
+                <div class="card" style="background-image: url('./images/13.jpg');">
+                    <p>Desert</p>
+                </div>
+            </a>
+        </div>
     </div>
-    <div class="card column">
-      <p>Beaverages</p>
-    </div>
-    <div class="card column">
-      <p>full course meal</p>
-    </div>
-    <div class="card column">
-      <p>desert</p>
-    </div>
-  </div>
   <!-- Explore more -->
   <!-- Photo gallery -->
   <h1 class="header">Our gallery</h1>
-<div class="row"> 
-  <div class="column">
-    <img src="./images/11.jpg" style="width:100%">
+  <div class="row">
+    <div class="column">
+      <img src="./images/11.jpg" style="width:100%">
+    </div>
+    <div class="column">
+      <img src="./images/12.jpg" style="width:100%">
+      <img src="./images/13.jpg" style="width:100%">
+    </div>
+    <div class="column">
+      <img src="./images/15.jpg" style="width:100%">
+    </div>
+    <div class="column">
+      <img src="./images/14.jpg" style="width:100%">
+    </div>
   </div>
-  <div class="column">
-  <img src="./images/12.jpg" style="width:100%">
-  <img src="./images/13.jpg" style="width:100%">
-  </div>
-  <div class="column">
-  <img src="./images/15.jpg" style="width:100%">
-  </div>
-  <div class="column">
-  <img src="./images/14.jpg" style="width:100%">
-  </div>
-</div>
   <!-- Photo gallery -->
-  
+
   <!-- footer -->
   <div class="footer">
     <p>Shop name.com</p>
